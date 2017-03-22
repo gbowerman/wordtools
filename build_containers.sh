56fe5c575b0558466cb13b2aa9937a49672d907d
Guy Bowerman <guybo@outlook.com>
2017-03-22 14:56:52 -0700

    Fixing data layer and adding container build script

---
+++ build_containers.sh
@@ -0,0 +1,10 @@
+# build containers
+cd containers
+cd api_layer
+docker build -t wordtools_api .
+cd ../presentation_layer
+docker build -t wordtools_presentation .
+cd ../data_layer
+./gen_word_data.sh
+docker build -t wordtools_data .
+cd ..
--- containers/data_layer/createdb.sql
+++ containers/data_layer/createdb.sql
@@ -1,8 +1,8 @@
--- MySQL database script to generate the worddb database 
+-- MySQL database script to generate the worddb database
 -- Assumes a local file called en.csv containing a comma seperated list which looks like:
 -- 0,word,4
 -- where the last column is the word length.
--- You will also need to create a user with access to this db, which you will add to the 
+-- You will also need to create a user with access to this db, which you will add to the
 -- dbconfig.json file used by the word tools app. Example below assumes 'worduser'.
 -- DROP DATABASE worddb;
 -- CREATE DATABASE worddb;
@@ -19,5 +19,3 @@ ALTER TABLE words ADD UNIQUE word_idx(word);
 LOAD DATA INFILE './en.csv' INTO TABLE words
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
-CREATE USER 'worduser'@'localhost' IDENTIFIED BY 'W0rdApp,User01';
-GRANT SELECT ON worddb.words TO worduser@localhost;
