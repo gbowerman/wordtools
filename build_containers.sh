8f70d939a17c1bd7b5c2d7d3f8ca3efb19029090
Guy Bowerman <guybo@outlook.com>
2017-03-22 15:24:46 -0700

    Fix build/run script typos

--- build_containers.sh
+++ build_containers.sh
@@ -3,7 +3,7 @@ cd containers
 cd data_layer
 ./gen_word_data.sh
 docker build -t wordtools_data .
-cd api_layer
+cd ../api_layer
 docker build -t wordtools_api .
 cd ../presentation_layer
 docker build -t wordtools_presentation .
--- run_containers.sh
+++ run_containers.sh
@@ -1,8 +1,8 @@
 # run containers
 cd containers
-cd ../data_layer
+cd data_layer
 docker run -d -t -p 3306:3306 -e MYSQL_ROOT_PASSWORD=SqlD0ckerTest  -e MYSQL_USER=W0rdApp,User01 -e MYSQL_DATABASE=worddb -d wordtools_data
-cd api_layer
+cd ../api_layer
 docker run -d -t -p 8081:8081 wordtools_api
 cd ../presentation_layer
 docker run -d -t -p 8080:8080 wordtools_presentation
