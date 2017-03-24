3464606a7f806de9c3006131bbaa0637f38da66b
Guy Bowerman <guybo@outlook.com>
2017-03-23 17:37:17 -0700

    Complete containerization

--- worddb/gen_word_data.sh
+++
@@ -1,8 +0,0 @@
-# gen_word_data.sh - use this script to download the word list of your choice
-# This example uses Words - the word list behind Letterpress
-# Words is licensed under Create Commons - see: https://github.com/atebits/Words/blob/master/LICENSE
-# Should the link not work at some point, replace it with another working link to a line separated
-# list of words.
-curl https://raw.githubusercontent.com/atebits/Words/master/Words/en.txt > en.txt
-python3 ./line2csv.py en.txt > en.csv
-rm en.txt
