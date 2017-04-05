a75809b04d5a66149d74f04e1cbb2a77e0595259
Guy Bowerman <guybo@outlook.com>
2017-04-05 11:20:16 -0700

    Improving ci/cd

--- build_containers.sh
+++ build_containers.sh
@@ -3,12 +3,12 @@ cd containers
 cd data_layer
 bash ./gen_word_data.sh
 docker build -t sendmarsh/wordtools-data .
+docker push sendmarsh/wordtools-data
 cd ../api_layer
 docker build -t sendmarsh/wordtools_api .
+docker push sendmarsh/wordtools-api
 cd ../presentation_layer
 docker build -t sendmarsh/wordtools-presentation .
-cd ../..
-# push the containers 
-docker push sendmarsh/wordtools-data
-docker push sendmarsh/wordtools-api
 docker push sendmarsh/wordtools-presentation
+cd ../..
+
--- killjobs.sh
+++ killjobs.sh
@@ -1,3 +1,6 @@
 # kill running containers
 docker-compose stop
 docker-compose rm --force
+docker rmi sendmarsh/wordtools-data
+docker rmi sendmarsh/wordtools-api
+docker rmi sendmarsh/wordtools-presentation
