f1f1666e5b108288766bafcacf5c427649297beb
Guy Bowerman <guybo@outlook.com>
2018-02-15 09:29:01 -0800

    Comments, formatting cleanup

--- commandline/randword.py
+++ commandline/randword.py
@@ -1,5 +1,4 @@
 '''Random word tool - display random dictionary words'''
-
 import argparse
 import random
 import sys
@@ -9,13 +8,14 @@ COUNT = 3
 MINLEN = 3
 WORDFILE = 'words.txt'
 
-'''loads a line seperated word file into an array'''
+
 def load_words(wordfile, minlen, maxlen):
+    '''loads a line seperated word file into an array'''
     wordlist = []
     try:
         with open(wordfile) as wf:
             for word in wf:
-                wordlen = len(word) - 1 # remove linefeed from word
+                wordlen = len(word) - 1  # remove linefeed from word
                 if wordlen >= minlen and (maxlen is None or wordlen <= maxlen):
                     wordlist.append(word[:-1])
     except FileNotFoundError:
@@ -23,8 +23,8 @@ def load_words(wordfile, minlen, maxlen):
     return wordlist
 
 
-'''returns a set of count random ints between 0 and < len'''
 def random_indexes(count, num_words):
+    '''returns the requested count of random ints between 0 and < num_words'''
     index_array = []
     for _ in range(count):
         random_num = random.randint(0, num_words - 1)
@@ -33,41 +33,42 @@ def random_indexes(count, num_words):
 
 
 def main():
-    '''Main routine.'''
-    # validate command line arguments
+    '''Main routine. Start by parsing args..'''
     arg_parser = argparse.ArgumentParser()
-
     arg_parser.add_argument(
-        '--count', '-n', action='store', type = int, help='Number of words')
+        '--count', '-n', action='store', type=int, help='Number of words')
     arg_parser.add_argument(
-        '--minlen', '-min', action='store',  type = int, help='minimum word length')
+        '--minlen', '-min', action='store',  type=int, help='minimum word length')
     arg_parser.add_argument(
-        '--maxlen', '-max', action='store',  type = int, help='maximum word length')
+        '--maxlen', '-max', action='store',  type=int, help='maximum word length')
     arg_parser.add_argument(
         '--wordfile', '-f', action='store', help='wordfile')
 
     args = arg_parser.parse_args()
-
-    count = args.count
-    minlen = args.minlen
-    maxlen = args.maxlen
-    wordfile = args.wordfile
-
-    if count is None:
+    if args.count is None:
         count = COUNT
-    if minlen is None:
+    else:
+        count = args.count
+    if args.minlen is None:
         minlen = MINLEN
-    if wordfile is None:
+    else:
+        minlen = args.minlen
+    if args.wordfile is None:
         wordfile = WORDFILE
+    else:
+        wordfile = args.wordfile
+    maxlen = args.maxlen
 
+    # load words from a file into a Python list
     wordlist = load_words(wordfile, minlen, maxlen)
-
     total_words = len(wordlist)
 
+    # print out random words from the list
     index_array = random_indexes(count, total_words)
     for index in index_array:
         print(wordlist[index])
     print('..out of ' + str(total_words) + ' possible words.')
+
 
 if __name__ == "__main__":
     main()
