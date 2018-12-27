92d32c5fda1bdcab79a598e425b277c9413d5197
Guy Bowerman <guybo@outlook.com>
2018-12-27 09:51:43 +0000

    Add anagram function

--- commandline/wordfind.py
+++ commandline/wordfind.py
@@ -1,15 +1,13 @@
 '''Word fine - cheap and cheerful tool to find matching words with wildcards of the form w??d'''
 import argparse
-import random
 import sys
 # import time
 
 WORDFILE = 'words.txt'
 
 
-'''load words of matching length from a file into a list'''
 def load_words(wordfile, length):
-    '''loads a line seperated word file into an array'''
+    '''load words of matching length from a file into a list'''
     wordlist = []
     try:
         with open(wordfile) as wf:
@@ -22,8 +20,8 @@ def load_words(wordfile, length):
     return wordlist
 
 
-'''word_match - return true if partial word matches word'''
 def word_match(partial_word, word, wordlen):
+    '''return true if partial word matches word'''
     for x in range(wordlen):
         if partial_word[x] == '?':
             continue
@@ -32,29 +30,44 @@ def word_match(partial_word, word, wordlen):
     return True
 
 
+def wordfind(partial_word, wordfile):
+    '''finds matching words in a list - '?' is wild'''
+    wordlen = len(partial_word)
+    wordlist = load_words(wordfile, wordlen)
+    for word in wordlist:
+        if word_match(partial_word, word, wordlen):
+            print(word)
+
+
+def anagfind(anagram, wordfile):
+    '''finds an anagram by comparing two sorted strings'''
+    wordlen = len(anagram)
+    srtdarr = sorted(anagram)
+    wordlist = load_words(wordfile, wordlen)
+    for word in wordlist:
+        if srtdarr == sorted(word):
+            print(word)
+
+
 def main():
     '''Main routine. Start by parsing args..'''
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
         '--word', '-w', action='store', type=str, help='A partial word with ? for missing letters')
     arg_parser.add_argument(
+        '--anagram', '-a', action='store', type=str, help='Word to look for anagrams of')
+    arg_parser.add_argument(
         '--wordfile', '-f', action='store', default=WORDFILE, help='wordfile')
 
     args = arg_parser.parse_args()
 
-    # load words from a file into a list
-    partial_word = args.word
-    wordlen = len(partial_word)
-    wordlist = load_words(args.wordfile, wordlen)
-
     # time execution
     # start_time = time.time()
 
-    for word in wordlist:
-        if len(word) != wordlen:
-            continue
-        if word_match(partial_word, word, wordlen):
-            print(word)
+    if args.word is not None:
+        wordfind(args.word, args.wordfile)
+    if args.anagram is not None:
+        anagfind(args.anagram, args.wordfile)
 
     # end_time = time.time()
     # print('Execution time: ' + str(end_time - start_time))
